877063c1b65e2ca03c2759bba54418d860e76dfa
Petter Nilsson <pettni@caltech.edu>
2019-05-01 00:08:35 -0700

    back/fwd swipe in firefox

---
+++ .config/back.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+swaymsg seat seat0 cursor press BTN_SIDE && swaymsg seat seat0 cursor release BTN_SIDE
---
+++ .config/forward.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+swaymsg seat seat0 cursor press BTN_EXTRA && swaymsg seat seat0 cursor release BTN_EXTRA
