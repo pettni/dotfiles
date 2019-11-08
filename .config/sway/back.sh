ec14c22b96667c540d6de3b600fc2a5c4b6e40a3
Petter Nilsson <pettni@caltech.edu>
2019-11-08 13:54:12 -0500

    unified i3 and sway configs, as well as libinput-gestures

--- .config/sway/back.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-swaymsg seat seat0 cursor press BTN_SIDE && swaymsg seat seat0 cursor release BTN_SIDE
--- .config/sway/forward.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-swaymsg seat seat0 cursor press BTN_EXTRA && swaymsg seat seat0 cursor release BTN_EXTRA
